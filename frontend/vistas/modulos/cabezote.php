507c5e22f15a434a6e83772b873dd85449ee2fba
AlejandraCanel <63206992+AlejandraCanel@users.noreply.github.com>
2020-05-14 21:57:33 -0600

    Creacion del navbar y footer

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -1,39 +1,63 @@
-<!--=====================================
-TOP
-======================================-->
 
-<div class="container-fluid">
-	
-	<div class="container">
-		
-		<div class="row">
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark gris scrolling-navbar fixed-top">
 
-			<h1>Cabezote</h1>
-	
-			<!--=====================================
-			SOCIAL
-			======================================-->
-
-
-			<!--=====================================
-			REGISTRO
-			======================================-->
-
-		
-			<!--=====================================
-			HEADER
-			======================================-->
+      <div class="container">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
 
 
-			<!--=====================================
-			CARRITO DE COMPRAS
-			======================================-->
+          <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+              <a href="https://www.facebook.com" class="nav-link" target="_blank">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
 
-			
-		<!--=====================================
-		CATEGORÍAS
-		======================================-->
+            <li class="nav-item">
+              <a href="https://twitter.com" class="nav-link" target="_blank">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
 
-		
+            <li class="nav-item">
+              <a href="https://youtube.com" class="nav-link" target="_blank">
+                <i class="fa fa-youtube"></i>
+              </a>
+            </li>
 
+            <li class="nav-item">
+              <a href="https://instagram.com" class="nav-link" target="_blank">
+                <i class="fa fa-instagram"></i>
+              </a>
+            </li>
+
+            
+          </ul>
+
+          <ul class="navbar-nav nav-flex-icons">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Inicio |<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"> Registro</a>
+            </li>
+             
+          </ul>
+        </div>
+      </div>
+       
+      </nav>
+
+      <section class="fondo">
+        
+
+      </section>
+
+
+</body>
