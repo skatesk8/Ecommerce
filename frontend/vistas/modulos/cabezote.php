9db749b524a347c152b1a883a484e727ecc57267
AlejandraCanel <63206992+AlejandraCanel@users.noreply.github.com>
2020-05-15 18:13:36 -0600

    Creacion de modal ingresar y registro

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -39,19 +39,68 @@
 
             
           </ul>
-
-          <ul class="navbar-nav nav-flex-icons">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Inicio |<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"> Registro</a>
-            </li>
-             
-          </ul>
-        </div>
+<!--------------------------------- INGRESAR------------------------------------------------------------------------------------------------>
+          <ul class="navbar-nav nav-flex-icons">  
+          <button type="button"class="btn btn-warning" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Ingresar</button>
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title"  id="exampleModalLabel">Ingresar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-       
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Correo Electronico:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Password:</label>
+            <input type="password" name="password" id="password" class="form-control" /> 
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-warning">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--------------------------------- INGRESAR------------------------------------------------------------------------------------------------>
+
+<!--------------------------------- REGISTRAR------------------------------------------------------------------------------------------------>
+<button type="button" name="login"  class="btn btn-warning" data-toggle="modal" data-target="#loginModal">Registrar</button>  
+               
+<div id="loginModal" class="modal fade" role="dialog">  
+      <div class="modal-dialog">  
+   <!-- Modal content-->  
+           <div class="modal-content">  
+                <div class="modal-header">  
+                <h4 class="modal-title">Registrar</h4>
+                     <button type="button" class="close" data-dismiss="modal">&times;</button>  
+                       
+                </div>  
+                <div class="modal-body">  
+                     <label>Username</label>  
+                     <input type="text" name="username" id="username" class="form-control" />  
+                     <br />  
+                     <label>Username</label>  
+                     <input type="text" name="username" id="username" class="form-control" />  
+                     <br /> 
+                     <label>Password</label>  
+                     <input type="password" name="password" id="password" class="form-control" />  
+                     <br /> 
+                     <button type="button" name="login_button" id="login_button" class="btn btn-warning">Login</button>  
+                </div>  
+           </div>  
+      </div>  
+ </div>  
+</ul>
+<!--------------------------------- REGISTRAR------------------------------------------------------------------------------------------------>
+
       </nav>
 
       <section class="fondo">
