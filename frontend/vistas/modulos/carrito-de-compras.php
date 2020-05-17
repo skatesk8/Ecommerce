33e808b8a849179ee63ed9bd95a055d85939e142
Lucia Perez <lucia.01rosariope@gmail.com>
2020-05-16 21:32:32 -0600

    carrito de compras

--- frontend/vistas/modulos/carrito-de-compras.php
+++ frontend/vistas/modulos/carrito-de-compras.php
@@ -2,14 +2,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" href="carrito.css">
+    <link rel="stylesheet" href="vistas/css/carrito.css">
 
     <!--JS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="incremento.js"></script>
-		
+    
 <!--=====================================
 TABLA CARRITO DE COMPRAS
 ======================================-->
