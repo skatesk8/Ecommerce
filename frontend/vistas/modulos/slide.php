1e2a9c5c7234b0c369feee7a79c9c70eef0568d2
AlejandraCanel <63206992+AlejandraCanel@users.noreply.github.com>
2020-05-18 23:00:11 -0600

    Creacion de slider

--- frontend/vistas/modulos/slide.php
+++ frontend/vistas/modulos/slide.php
@@ -2,23 +2,34 @@
 SLIDESHOW  
 ======================================-->
 
-<div class="container-fluid">
-	
-	<div class="row">
-
-		<h1>Slider</h1>
-		
-		<!--=====================================
-		DIAPOSITIVAS
-		======================================-->
-
-
-		<!--=====================================
-		PAGINACIÓN
-		======================================-->
-
-		<!--=====================================
-		FLECHAS
-		======================================-->
-	</div>	
-</div>
+<div id="carousel1" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Primer slide"><title>Placeholder</title><rect width="100%" height="100%" fill="#2196f3"></rect><text x="45%" y="50%" fill="#eceff1" dy=".3em">Primer slide</text></svg>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Segundo slide"><title>Placeholder</title><rect width="100%" height="100%" fill="#f50057"></rect><text x="45%" y="50%" fill="#eceff1" dy=".3em">Segundo slide</text></svg>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Tercer slide"><title>Placeholder</title><rect width="100%" height="100%" fill="#ff3d00"></rect><text x="45%" y="50%" fill="#eceff1" dy=".3em">Tercer slide</text></svg>
+            </div>
+            </div>
+            
+            <!--Controles NEXT y PREV-->
+            <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            <!--Controles de indicadores-->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel1" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel1" data-slide-to="1"></li>
+                <li data-target="#carousel1" data-slide-to="2"></li>
+            </ol>
+            
+        </div>
+      <br>
--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -35,6 +35,8 @@
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+	
+	
 	<!--=====================================
 	HOJAS DE ESTILO PERSONALIZADAS
 	======================================-->
@@ -76,7 +78,18 @@
 
 	<!--Scripts Guadalpe:  para el grid-list y para marcar como favorito-->
 	<script src="vistas/js/complemento.js"></script>
-    <script src="vistas/js/favorito.js"></script>
+	<script src="vistas/js/favorito.js"></script>
+	
+	    <!-- Optional JavaScript ALE -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS SLIDER ALE-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <script>
+       //$('.carousel').carousel();
+    </script>  
+      
 
 </head>
 
