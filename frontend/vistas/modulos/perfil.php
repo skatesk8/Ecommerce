3c39a1c78f01726e27ed4499ee3870365540c95e
Lucia Perez <lucia.01rosariope@gmail.com>
2020-05-22 11:37:35 -0600

    pestañas actualizacion

--- frontend/vistas/modulos/perfil.php
+++ frontend/vistas/modulos/perfil.php
@@ -2,6 +2,7 @@
  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="vistas/modulos/css/perfil.css">
 
     <!--FONTAWESOME-->
 
@@ -13,7 +14,19 @@
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
  
+    <script>
+       if ($("#nav-profile-tab").attr("checked")){
+            $("#nav-home-tab").css("display", "block");
+        }else{
+        $("#nav-home-tab").css("display", "none");
+        }
 
+        if ($("#nav-home-tab").attr("checked")){
+            $("#nav-profile").css("display", "block");
+        }else{
+        $("#nav-profile").css("display", "none");
+        }
+    </script>
 
 
 
@@ -35,7 +48,7 @@ SECCIÓN PERFIL
 				<nav>
 					<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
 						<a class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
-							aria-controls="nav-home" aria-selected="true"><i class="fa fa-list-ul"></i> Mis compras</a>
+							aria-controls="nav-home" aria-selected="true"><i class="fa fa-list-ul"></i> MIS COMPRAS</a>
 						<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
 							role="tab" aria-controls="nav-profile" aria-selected="false"><i class="fa fa-list-ul"></i>
 							MI LISTA DE DESEOS</a>
@@ -51,126 +64,46 @@ SECCIÓN PERFIL
                     <!--=====================================
 			            PESTAÑA DE MIS COMPRAS
 			        ======================================-->
-					<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                        <br> <br> <br>
-						<div id="products" class="row view-group">
-                        <div class="item col-xs-4 col-lg-3">
-                            <div class="thumbnail card">
-                                <div class="img-event">
-                                    <div class="img-box"> <!--Div para efecto zoom-hover-->
-                                    <img class="group list-group-image img-fluid" src="vistas/img/productos/accesorios/accesorio01.jpg" alt="" />
-                                </div>
-                                </div>
-                                <div class="caption card-body">
-                                    <h6>465</h6>
-                                    <h4 class="group card-title inner list-group-item-heading">
-                                        Anillo Diamantes</h4>
-                                    <p class="group inner list-group-item-text">
-                                    GRATIS</p>
-                                    <div class="row">
-                                        <div class="col-xs-12 col-md-6">
-                                            <p class="lead">
-                                                </p>
-                                        </div>
-                                        <div class="col-xs-2 col-md-6">
-                                            <a class="btn btn-outline-secondary glyphicon glyphicon-eye-open" href="#"></a><!--BOTON DE VER DETALLES DE PRODUC.-->
-                                            <a class="mi-boton btn btn-warning glyphicon glyphicon-heart" href="#"></a><!--BOTON QUE AGREGA A FAVORITOS.-->
-                                        </div>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
+					<div class="tab-pane fade" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                         <br> 
 
-                            <div class="item col-xs-4 col-lg-3">
-                                <div class="thumbnail card">
-                                    <div class="img-event">
-                                    <div class="img-box">
-                                        <img class="group list-group-image img-fluid" src="vistas/img/productos/accesorios/accesorio02.jpg" alt="" />
-                                    </div>
-                                    </div>
-                                    <div class="caption card-body">
-                                        <h6>205</h6>
-                                        <h4 class="group card-title inner list-group-item-heading">
-                                            Bolso Deportivo Gris</h4>
-                                        <p class="group inner list-group-item-text">
-                                        GRATIS</p>
-                                        <div class="row">
-                                            <div class="col-xs-12 col-md-6">
-                                                <p class="lead">
-                                                    </p>
-                                            </div>
-                                            <div class="col-xs-12 col-md-6">
-                                                <a class="btn btn-outline-secondary glyphicon glyphicon-eye-open" href="#"></a><!--BOTON DE VER DETALLES DE PRODUC.-->
-                                                <a class="mi-boton btn btn-warning glyphicon glyphicon-heart" href="#"></a><!--BOTON QUE AGREGA A FAVORITOS.-->
-                                            </div>
-                                        </div>
+                         <div class="card mt-2">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img class="img-fluid" src="C:/Users/Lucia/Desktop/imagenes/KATYA2.jpg" alt="">
+                                </div>
+                                <div class="col-md-8">
+                                    <h2 class="card-title mt-3 text-uppercase">UNDERGROUD</h2>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima assumenda nemo, autem fuga non, ullam recusandae earum nesciunt omnis vitae quod molestias id accusamus nobis dolor dolorum. Quas, error et!</p>
+                                    <button class="btn btn-outline-secondary "> VER EL LIBRO</button>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="stars">
+                                        <form action="">
+                                            <input class="star star-5" id="star-5" type="radio" name="star"/>
+                                            <label class="star star-5" for="star-5"></label>
+                                            <input class="star star-4" id="star-4" type="radio" name="star"/>
+                                            <label class="star star-4" for="star-4"></label>
+                                            <input class="star star-3" id="star-3" type="radio" name="star"/>
+                                            <label class="star star-3" for="star-3"></label>
+                                            <input class="star star-2" id="star-2" type="radio" name="star"/>
+                                            <label class="star star-2" for="star-2"></label>
+                                            <input class="star star-1" id="star-1" type="radio" name="star"/>
+                                            <label class="star star-1" for="star-1"></label>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="item col-xs-4 col-lg-3">
-                                <div class="thumbnail card">
-                                    <div class="img-event">
-                                        <div class="img-box">
-                                        <img class="group list-group-image img-fluid" src="vistas/img/productos/accesorios/accesorio03.jpg" alt="" />
-                                    </div>
-                                    </div>
-                                    <div class="caption card-body">
-                                        <h6>205</h6>
-                                        <h4 class="group card-title inner list-group-item-heading">
-                                            Bolso Militar</h4>
-                                        <p class="group inner list-group-item-text">
-                                        GRATIS</p>
-                                        <div class="row">
-                                            <div class="col-xs-12 col-md-6">
-                                                <p class="lead">
-                                                    </p>
-                                            </div>
-                                            <div class="col-xs-12 col-md-6">
-                                                <a class="btn btn-outline-secondary glyphicon glyphicon-eye-open" href="#"></a><!--BOTON DE VER DETALLES DE PRODUC.-->
-                                                <a class="mi-boton btn btn-warning glyphicon glyphicon-heart" href="#"></a><!--BOTON QUE AGREGA A FAVORITOS.-->
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="item col-xs-4 col-lg-3">
-                                <div class="thumbnail card">
-                                    <div class="img-event">
-                                    <div class="img-box">
-                                        <img class="group list-group-image img-fluid" src="vistas/img/productos/accesorios/accesorio04.jpg" alt="" />
-                                    </div>
-                                    </div>
-                                    <div class="caption card-body">
-                                        <h6>325</h6>
-                                        <h4 class="group card-title inner list-group-item-heading">
-                                            Collar Diamantes</h4>
-                                        <p class="group inner list-group-item-text">
-                                        GRATIS </p>
-                                        <div class="row">
-                                            <div class="col-xs-12 col-md-6">
-                                                <p class="lead">
-                                                    </p>
-                                            </div>
-                                            <div class="col-xs-12 col-md-6">
-                                                <a class="btn btn-outline-secondary glyphicon glyphicon-eye-open" href="#"></a><!--BOTON DE VER DETALLES DE PRODUC.-->
-                                                <a class="mi-boton btn btn-warning glyphicon glyphicon-heart" href="#"></a><!--BOTON QUE AGREGA A FAVORITOS.-->
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                         </div>
+                        <br>
                     </div>
 
 			        <!--=====================================
 			        PESTAÑA DESEOS
                     ======================================-->
-					<div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+					<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
 						
-                        <br> <br> <br>
+                        <br> <br>
 						<div id="products" class="row view-group">
 							<div class="item col-xs-4 col-lg-3">
 								<div class="thumbnail card">
@@ -302,18 +235,28 @@ SECCIÓN PERFIL
 								</div>
 							</div>
 						</div>
-					</div>
+                    </div>
+                    
 					<!--=====================================
 			        PESTAÑA PERFIL
 			        ======================================-->
 					<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-						Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim
-						occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit
-						dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse
-						consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod
-						tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non
-						adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat
-						ex.
+                        <form>
+                        <div class="input-group flex-nowrap">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping">@</span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>
+                        </form>
+                            <div class="col-xs-5">
+                            <label for="ex3">col-xs-4</label>
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping">@</span>
+                            </div>
+                            <input class="form-control" id="ex3" type="text">
+                        </div>
+                        
 					</div>
 					<!--=====================================
 			        PESTAÑA VER OFERTAS
