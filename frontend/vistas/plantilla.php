3c49a2ba381cd2c94c9d1ff75a3673a809ecb11b
Kevin Tobar <tobar0072@gmail.com>
2020-06-06 23:46:13 -0600

    inclusion de api de facebook

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -201,7 +201,7 @@ if(isset($_GET["ruta"])){
 
 	$rutaCategorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
 
-	if($rutas[0]  == $rutaCategorias["ruta"]){
+	if($rutas[0] == $rutaCategorias["ruta"]){
 
 		$ruta = $rutas[0];
 
@@ -283,11 +283,15 @@ include "modulos/footer.php";
 <!--=====================================
 JAVASCRIPT PERSONALIZADO
 ======================================-->
-<script src="<?php echo $url; ?>vistas/js/usuarios.js"></script>
+
 <script src="<?php echo $url; ?>vistas/js/cabezote.js"></script>
 <script src="<?php echo $url; ?>vistas/js/plantilla.js"></script>
 <script src="<?php echo $url; ?>vistas/js/slide.js"></script>
+<script src="<?php echo $url; ?>vistas/js/buscador.js"></script>
 <script src="<?php echo $url; ?>vistas/js/infoproducto.js"></script>
+<script src="<?php echo $url; ?>vistas/js/usuarios.js"></script>
+<script src="<?php echo $url; ?>vistas/js/registroFacebook.js"></script>
+<script src="<?php echo $url; ?>vistas/js/carrito-de-compras.js"></script>
 <script src="<?php echo $url; ?>vistas/js/visitas.js"></script>
 
 <!--=====================================
@@ -296,7 +300,27 @@ https://developers.facebook.com/
 
 <?php echo $plantilla["apiFacebook"]; ?>
 
+
 <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '585104619090628',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
   /*=============================================
 	COMPARTIR EN FACEBOOK
